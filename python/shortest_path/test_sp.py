d77039b8989fb1d5bd1ee810e88904d1130ef787
ygbingo <yanhuibin315@163.com>
2023-12-31 15:40:07 +0800

    dijkstra

--- python/shortest_path/test_sp.py
+++ python/shortest_path/test_sp.py
@@ -1,5 +1,6 @@
 from shortest_path.graph import Edge, Graph
 from shortest_path.bellman_ford import bellman_ford
+from shortest_path.dijkstra import dijkstra
 
 
 def test_bellman_ford():
@@ -15,3 +16,18 @@ def test_bellman_ford():
  
     # function call
     bellman_ford(g, 0)
+
+
+def test_dijkstra():
+    """ 算法书中有向边的举例 """
+    graph = Graph(5)
+    graph.add_edge(Edge("s", "y", 5))
+    graph.add_edge(Edge("s", "t", 10))
+    graph.add_edge(Edge("t", "x", 1))
+    graph.add_edge(Edge("y", "t", 3))
+    graph.add_edge(Edge("t", "y", 2))
+    graph.add_edge(Edge("y", "x", 9))
+    graph.add_edge(Edge("y", "z", 2))
+    graph.add_edge(Edge("x", "z", 4))
+    graph.add_edge(Edge("z", "x", 6))
+    graph.add_edge(Edge("z", "s", 7))
