7fe1e86cbcec43310671f538d2b9589e2a2ba20c
ygbingo <yanhuibin315@163.com>
2024-01-02 14:50:25 +0800

    leetcode 466

--- python/dynamic_programming/test_DP.py
+++ python/dynamic_programming/test_DP.py
@@ -13,3 +13,13 @@ def test_cut_rod():
 def test_lvp():
     s = "(()))())("
     assert longest_valid_parentheses(s) == 4
+
+
+from getMaxRepetitions_466 import get_max_repetitions
+
+
+def test_466():
+    s1, n1, s2, n2 = "acb", 4, "ab", 2
+    assert get_max_repetitions(s1, n1, s2, n2) == 2
+    s1, n1, s2, n2 = "acb", 1, "acb", 1
+    assert get_max_repetitions(s1, n1, s2, n2) == 1
